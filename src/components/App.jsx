9049913dc36d1fc0270e4949a8a83c23cbd7d818
Juan Blanco <juanblancodev@gmail.com>
2024-04-15 18:34:00 -0700

    wrapper added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,20 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin: 80px auto;
+  width: 85%;
+  height: 470px;
+
+  border-radius: 15px;
+  border: 1px solid var(--color-black);
+  box-shadow: 10px 10px 0 0 var(--color-black);
+`
+
 const App = () => {
   return (
-    <div>App</div>
+    <Wrapper className='bg-white'>
+
+    </Wrapper>
   )
 }
 
