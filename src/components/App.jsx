2b0008532a932c551989924e6268e0dcaa6fde17
Juan Blanco <juanblancodev@gmail.com>
2024-04-15 18:42:30 -0700

    ilustration add for mobile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,31 @@
 import styled from 'styled-components'
+import Ilustration from './Ilustration'
 
 const Wrapper = styled.div`
   margin: 80px auto;
   width: 85%;
   height: 470px;
 
+  padding: 25px;
+
   border-radius: 15px;
   border: 1px solid var(--color-black);
   box-shadow: 10px 10px 0 0 var(--color-black);
 `
 
+const Content = styled.div`
+  border: 1px solid red;
+
+  width: 100%;
+  height: 100%;
+`
+
 const App = () => {
   return (
     <Wrapper className='bg-white'>
-
+      <Content>
+        <Ilustration />
+      </Content>
     </Wrapper>
   )
 }
---
+++ src/components/Ilustration.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import ilustrationSVG from '../assets/illustration-article.svg'
+
+const SVGContainer = styled.div`
+  width: 100%;
+  height: 200px;
+  overflow: hidden;
+
+  border-radius: 10px;
+`
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+
+const Ilustration = () => {
+  return (
+    <SVGContainer>
+      <Img src={ilustrationSVG} alt="Ilustration" />
+    </SVGContainer>
+  )
+}
+
+export default Ilustration
