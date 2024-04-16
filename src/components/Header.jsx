fc79abdad7c89d470f9fb5a1f7a09f678c3768a7
Juan Blanco <juanblancodev@gmail.com>
2024-04-15 19:56:28 -0700

    illustration container placed inside the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components'
+import Ilustration from './Ilustration'
 import Label from './Label'
 
 const Container = styled.header`
   display: grid;
-  row-gap: 15px;
+  row-gap: 20px;
 `
 
 const Header = () => {
   return (
     <Container>
+      <Ilustration />
       <Label info="Learning" />
       <p className='fw-normal'>Published 21 Dec 2023</p>
       <h1 className='active fw-bold fs-medium nowarp'>HTML & CSS foundations</h1>
